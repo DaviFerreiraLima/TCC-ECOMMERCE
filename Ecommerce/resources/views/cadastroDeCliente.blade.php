75c0e73151787f9609ddadbe708b61d9b7c53dda
Zerfallener-Succellus <pedrolucf@gmail.com>
2023-01-05 19:10:32 -0300

    Front end do cadastro de usuario

--- Ecommerce/resources/views/cadastroDeCliente.blade.php
+++ Ecommerce/resources/views/cadastroDeCliente.blade.php
@@ -1,44 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/cadastro.css">
     <script scr="/js/scripts.js"></script>
     <title>Cadastro do Usuário</title>
 </head>
+
 <body>
     <header>
 
     </header>
-    <main>  
+    <main>
+    <div class="seta"></div>
+    <h1 class="voltar">Voltar</h1>
         <div class="right-tabela">
-            <div class="input">
+            <div >
                 <div class="personalizacao-tabelas">
                     <form class="textfield" action="/cliente/cadastrar" method="POST">
                         @csrf
 
-                        <label for="nomeDoCliente">Nome</label>
-                        <input type="text" name="nomeDoCliente" id="nomeDoCliente" placeholder="Seu Nome">
+                        <h1>Cadastre-se</h1>
 
-                        <label for="emailDoCliente">Email</label>
-                        <input type="email" name="emailDoCliente" id="emailDoCliente" placeholder="Email">
+                       
 
-                        <label for="cpfDoCliente">CPF</label>
-                        <input type="text" name="cpfDoCliente" id="cpfDoCliente" placeholder="CPF">
+                        <div class="barra_progresso">
+                            <span></span>
+                            <span></span>
+                            <span></span>
 
-                        <label for="telefoneDoCliente">Telefone</label>
-                        <input type="text" name="telefoneDoCliente" id="telefoneDoCliente" placeholder="Telefone">
+                        </div>
 
-                        <label for="dataDeNascimento">Data de Nascimento</label>
-                        <input type="date" name="dataDeNascimento" id="dataDeNascimento">
+                        <br>
+                        <div class="barra_divisao">
+<span></span>
+<span></span>         
+<span></span>               
+                        </div>
+                        
+                        <br>
 
-                        <label for="senhaDoCliente">Senha</label>
-                        <input type="password" name="senhaDoCliente" id="senhaDoCliente">
+                        <div class="inputs">
+                        <input class="input" type="text" name="nomeDoCliente" id="nomeDoCliente" placeholder="Seu Nome">
+
+                        
+                        <input class="input" type="email" name="emailDoCliente" id="emailDoCliente" placeholder="Email">
+                        
+                        
+                        <input class="input" type="text" name="cpfDoCliente" id="cpfDoCliente" placeholder="CPF">
+                        
+                        
+                        <input class="input" type="text" name="telefoneDoCliente" id="telefoneDoCliente" placeholder="Telefone">
+                        
+                        
+                        <input class="input" type="date" name="dataDeNascimento" id="dataDeNascimento" placeholder="Data de Nascimento">
+                        
+                       
+                        <input class="input" type="password" name="senhaDoCliente" id="senhaDoCliente" placeholder="Senha">
+                        <br>
+                        </div>
+
 
                         <div>
-                            <input type="submit" class="btn-login" value="cadastrarCliente">
+                            <input type="submit" class="botão" value="Continuar">
                         </div>
 
                     </form>
@@ -48,4 +75,5 @@
     </main>
     <footer></footer>
 </body>
+
 </html>
